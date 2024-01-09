cd61eeacca53bb73d294135aea0745430d775a74
luiscarbonel1991 <devhighlevel@gmail.com>
2024-01-09 17:26:19 -0600

    Update MainNav with navConfig map

--- app/(marketing)/_components/main-nav.tsx
+++ app/(marketing)/_components/main-nav.tsx
@@ -4,37 +4,33 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { navConfig } from "@/config/nav-config";
 
 const MainNav = () => {
 
     const pathname = usePathname();
 
     return (
-        <nav className="md:ml-auto justify-end  md:justify-between w-full flex items-center gap-x-2">
-            <Button asChild variant="ghost" size="sm"
-                className={cn(
-                    "hidden md:flex rounded-3xl transition-transform hover:text-foreground/80 hover:translate-y-[-1px] hover:scale-[1.01] hover:border-b-2 hover:border-primary",
-                    pathname === "/services" ? "text-foreground" : "text-foreground/60"
-                )}
-            >
-                <Link href={"/services"}>Services</Link>
-            </Button>
-            <Button asChild variant="ghost" size="sm"
-                className={cn(
-                    "hidden md:flex rounded-3xl transition-transform hover:text-foreground/80 hover:translate-y-[-1px] hover:scale-[1.01] hover:border-b-2 hover:border-primary",
-                    pathname === "/testimonials" ? "text-foreground" : "text-foreground/60"
-                )}
-            >
-                <Link href={"/testimonials"}>Testimonial</Link>
-            </Button>
-            <Button asChild variant="ghost" size="sm" 
-                className={cn(
-                    "hidden md:flex rounded-3xl transition-transform hover:text-foreground/80 hover:translate-y-[-1px] hover:scale-[1.01] hover:border-b-2 hover:border-primary",
-                    pathname === "/team" ? "text-foreground" : "text-foreground/60"
-                )}
-            >
-                <Link href={"/team"}>Team</Link>
-            </Button>
+        <nav className="md:ml-auto justify-end  w-full flex items-center gap-x-2">
+            {
+                navConfig.main
+                .filter( item => item.href != '/contact')
+                .map((item, index) => {
+                    return (
+                        <Button asChild variant="ghost" size="sm"
+                            className={cn(
+                                "hidden md:flex rounded-3xl transition-transform hover:text-foreground/80 hover:translate-y-[-1px] hover:scale-[1.01] hover:border-b-2 hover:border-primary",
+                                pathname === item.href ? "text-foreground" : "text-foreground/60"
+                            )}
+                            key={index}
+                        >
+                            <Link href={item.href ?? '/'}>{item.title}</Link>
+                        </Button>
+                    )
+                }
+                )
+            }
+
             <Button asChild variant="default" size="sm" className="rounded-3xl shadow-2xl transition-transform hover:translate-y-[-1px] hover:scale-[1.01] hover:border-b-2 hover:border-primary">
                 <Link href={"/contact"}>Contact us</Link>
             </Button>
