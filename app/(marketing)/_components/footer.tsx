26eab8f1628c82147ee08491eb9e8ee3cceaa4bb
luiscarbonel1991 <devhighlevel@gmail.com>
2024-01-10 11:41:42 -0600

    Fix navbar, hero and footer with container

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,30 +1,31 @@
-import { Button } from "@/components/ui/button";
-import { siteConfig } from "@/config/site"
-import { Icons } from "./icons";
+import {Button} from "@/components/ui/button";
+import {siteConfig} from "@/config/site"
+import {Icons} from "./icons";
 
 import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
-         <div className="mr-4 hidden md:flex w-full">
-            <Link href="/" className="mr-6 flex items-center space-x-2">
-                <Icons.logo className="h-6 w-6" />
-                <span className="hidden font-bold sm:inline-block">
+        <footer className="container flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
+            <div className="mr-4 hidden md:flex w-full">
+                <Link href="/" className="mr-6 flex items-center space-x-2">
+                    <Icons.logo className="h-6 w-6"/>
+                    <span className="hidden font-bold sm:inline-block">
                     {siteConfig.name}
                 </span>
-            </Link>
+                </Link>
             </div>
-        <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-          <Button variant="ghost" size="sm">
-            Privacy Policy
-          </Button>
-          <Button variant="ghost" size="sm">
-            Terms & Conditions
-          </Button>
-        </div>
-      </footer>
+            <div
+                className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+                <Button variant="ghost" size="sm">
+                    Privacy Policy
+                </Button>
+                <Button variant="ghost" size="sm">
+                    Terms & Conditions
+                </Button>
+            </div>
+        </footer>
     )
-  }
+}
 
-    export default Footer;
+export default Footer;
--- app/(marketing)/_components/hero.tsx
+++ app/(marketing)/_components/hero.tsx
@@ -1,87 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
-import {Card, CardContent} from "@/components/ui/card";
 
 
 export const Hero = () => {
 
     return (
-        /*<main className="grid lg:grid-cols-2 place-items-center pt-32 md:pb-24 px-4 md:px-10">
-            <picture className="py-6 md:order-1 hidden md:block">
-                <Image
-                    src="/static/images/marketing/hero_v3.png"
-                    alt="A Developer in front of a computer developing software"
-                    height={520}
-                    width={424}
-                    className="rounded-3xl shadow-md"
-                />
-            </picture>
-            <aside>
-                <Button variant="secondary" className="mb-2 rounded-tl-2xl rounded-br-2xl"
-                        size="sm">
-                    Welcome
-                </Button>
-                <h1 className="text-5xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
-                    Let’s make your product a success
-                </h1>
-                <p className="mt-6 text-xl text-gray-600 dark:text-gray-400">
-                    We are a software development studio that helps startups and enterprises build
-                    quality software to achieve growth and scale.
-                </p>
-                <div className="mt-6 flex flex-col sm:flex-row gap-3">
-                    <Button asChild variant="default" className="rounded-3xl" size="lg">
-                        <Link
-                            href="/contact"
-                            aria-label="Let's Talk"
-                        >
-                            Let's Talk
-                        </Link>
+
+        <>
+
+            <main className="container grid lg:grid-cols-2 place-items-center pt-32 md:pb-24 px-4 md:px-10">
+                <picture className="py-6 md:order-1 hidden md:block">
+                    <Image
+                        src="/static/images/marketing/hero_v3.png"
+                        alt="A Developer in front of a computer developing software"
+                        height={520}
+                        width={520}
+                        className="rounded-3xl shadow-md"
+                    />
+                </picture>
+                <aside>
+                    <Button variant="secondary" className="mb-2 rounded-tl-2xl rounded-br-2xl"
+                            size="sm">
+                        Welcome
                     </Button>
-                </div>
-            </aside>
-        </main>*/
-
-        <Card className="mx-auto max-w-7xl py-14 md:m-6 sm:m-1 shadow-md border-t-0 rounded-3xl">
-            <CardContent>
-                <div className="grid lg:grid-cols-2 place-items-center">
-                    <picture className="py-6 md:order-1 hidden md:block">
-                        <Image
-                            src="/static/images/marketing/hero_v3.png"
-                            alt="A Developer in front of a computer developing software"
-                            height={520}
-                            width={424}
-                            className="rounded-3xl shadow-md"
-                        />
-                    </picture>
-                    <aside className="px-4 md:px-10">
-                        <Button variant="secondary" className="mb-2 rounded-tl-2xl rounded-br-2xl"
-                                size="sm">
-                            Welcome
+                    <h1 className="text-5xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
+                        Let’s make your product a success
+                    </h1>
+                    <p className="mt-6 text-xl text-gray-600 dark:text-gray-400">
+                        We are a software development studio that helps startups and enterprises build
+                        quality software to achieve growth and scale.
+                    </p>
+                    <div className="mt-6 flex flex-col sm:flex-row gap-3">
+                        <Button asChild variant="default" className="rounded-3xl" size="lg">
+                            <Link
+                                href="/contact"
+                                aria-label="Let's Talk"
+                            >
+                                Let's Talk
+                            </Link>
                         </Button>
-                        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
-                            Let's make your product a success
-                        </h1>
-                        <p className="mt-6 text-xl text-gray-600 dark:text-gray-400">
-                            We are a software development studio that helps startups and enterprises build
-                            quality software to achieve growth and scale.
-                        </p>
-                        <div className="mt-6 flex flex-col sm:flex-row gap-3">
-                            <Button asChild variant="default" className="rounded-3xl" size="lg">
-                                <Link
-                                    href="/contact"
-                                    aria-label="Let's Talk"
-                                >
-                                    Let's Talk
-                                </Link>
-                            </Button>
-                        </div>
-                    </aside>
-                </div>
-            </CardContent>
-        </Card>
-
-
-    )
-        ;
+                    </div>
+                </aside>
+            </main>
+        </>
+    );
 }
