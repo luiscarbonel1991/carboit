1e2d18301ac5ed897009f03b10f935850ce4d4e4
luiscarbonel1991 <devhighlevel@gmail.com>
2024-01-09 23:34:40 -0600

    Update hero page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,11 @@
+import {Hero} from "@/app/(marketing)/_components/hero";
 
 
 const MarketingPage = () => {
     return (
-        <>
-        
-        </>
+      <>
+          <Hero/>
+      </>
     );
 }
 
