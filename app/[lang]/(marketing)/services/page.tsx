df0ec23d2da913cde55bbb68f6b31b252b6941cc
luiscarbonel1991 <devhighlevel@gmail.com>
2024-02-17 13:52:28 -0600

    Fix Locale in service page

--- app/[lang]/(marketing)/services/page.tsx
+++ app/[lang]/(marketing)/services/page.tsx
@@ -1,9 +1,19 @@
-import {HeadingContactClean} from "@/app/(marketing)/_components/heading-contact-clean";
+import {HeadingContactClean} from "@/app/[lang]/(marketing)/_components/heading-contact-clean";
+import {Locale} from "@/i18n-config";
+import {getDictionary} from "@/lib/dictionary";
 
+interface ServicesPageProps {
+    params: {
+        lang: Locale
+    }
+}
 
-const ServicesPage = () => {
+const ServicesPage = async ({
+                                params: {lang}
+                            }: ServicesPageProps) => {
+    const dictionary = await getDictionary(lang)
     return (
-        <HeadingContactClean/>
+        <HeadingContactClean dictionary={dictionary.landing}/>
     )
 }
 
