9a20ec6c05e614d06255869e2b122be00417b20b
luiscarbonel1991 <devhighlevel@gmail.com>
2024-03-07 12:08:16 -0600

    Test Google Tag Manager Integration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com analytics.google.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
