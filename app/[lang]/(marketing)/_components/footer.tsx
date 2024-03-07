ea545a2d069c2f8750b9135bbc3157ed059638a5
luiscarbonel1991 <devhighlevel@gmail.com>
2024-03-06 21:25:00 -0600

    Update footer.tsx

--- app/[lang]/(marketing)/_components/footer.tsx
+++ app/[lang]/(marketing)/_components/footer.tsx
@@ -1,10 +1,12 @@
-import {Button} from "@/components/ui/button";
 import {Icons} from "@/components/icons";
 
 import Link from "next/link";
 import {siteMetadata} from "@/config/site-metadata";
 import {Locale} from "@/i18n-config";
 import {redirectPathURL} from "@/lib/utils";
+import WhatsappButton from "@/components/whatsapp-button";
+import InstagramButton from "@/components/instagram-button";
+import FacebookButton from "@/components/facebook-button";
 
 interface FooterProps {
     lang: Locale
@@ -12,25 +14,71 @@ interface FooterProps {
 
 const Footer = ({lang}: FooterProps) => {
     return (
-        <footer className="container flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
-            <div className="mr-4 hidden md:flex w-full">
-                <Link href={redirectPathURL(lang, '/')} className="mr-6 flex items-center space-x-2">
-                    <Icons.logo className="h-6 w-6"/>
-                    <span className="hidden font-bold sm:inline-block">
-                    {siteMetadata.applicationName}
-                </span>
-                </Link>
-            </div>
-            <div
-                className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-                <Link href="/privacy-policy">
-                    <Button variant="ghost" size="sm">
-                        Privacy Policy
-                    </Button>
-                </Link>
-                {/* <Button variant="ghost" size="sm">
-                    Terms & Conditions
-                </Button>*/}
+
+
+        <footer className="mt-20 p-4 bg-white shadow-inner sm:p-6 dark:bg-gray-800 rounded-t-lg">
+            <div className="container mx-auto max-w-screen-xl">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <Link href={redirectPathURL(lang, '/')} className="mr-6 flex items-center space-x-2">
+                            <Icons.logo className="h-6 w-6"/>
+                            <span className="font-bold sm:inline-block">
+                                {siteMetadata.applicationName}
+                             </span>
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow
+                                us</h2>
+                            <ul className="text-gray-600 dark:text-gray-400">
+                                <li className="mb-4">
+                                    <Link href="https://www.instagram.com/carbo.it"
+                                          aria-label="Instagram"
+                                          target="_blank"
+                                          className="hover:underline ">Instagram</Link>
+
+                                </li>
+                                <li>
+                                    <Link
+                                        href="https://www.facebook.com/profile.php?id=61556544511279"
+                                        target={"_blank"}
+                                        className="hover:underline">
+                                        Facebook
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                            <ul className="text-gray-600 dark:text-gray-400">
+                                <li className="mb-4">
+                                    <Link href="/privacy-policy" className="hover:underline">
+                                        Privacy Policy
+                                    </Link>
+                                </li>
+
+                                {/*<li>
+                                    <Link href="/privacy-policy" className="hover:underline">
+                                        Terms &amp; Conditions
+                                    </Link>
+                                </li>*/}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+                <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} {` `}
+                <a
+                    href="https://carboit.com" className="hover:underline">Carboit™</a>. All Rights Reserved.
+            </span>
+                    <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+                        <WhatsappButton/>
+                        <InstagramButton/>
+                        <FacebookButton/>
+                    </div>
+                </div>
             </div>
         </footer>
     )
